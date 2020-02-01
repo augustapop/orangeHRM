4f9d39e4a699645edf8a18f2572aed6b35caa171
augustapop <augustapop@yahoo.com>
2020-02-01 19:42:29 +0200

    2nd commit
    2nd commit

---
+++ src/main/java/diverse/Download.java
@@ -0,0 +1,45 @@
+package diverse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class Download {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Drivers//chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/FileDownload.html");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.id("textbox")).sendKeys("text for txt file");
+		driver.findElement(By.id("createTxt")).click();
+		driver.findElement(By.id("link-to-download")).click();
+		driver.close();
+
+		//Additional settings for firefox browser;mime-type
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("browser.helperApps.neverAsk.saveToDisk","text/plain,application/pdf");
+		profile.setPreference("browser.download.manager.showWhenStarting",false);
+		profile.setPreference("pdfjs.disabled",true);
+		
+		FirefoxOptions option=new FirefoxOptions();
+		option.setProfile(profile);
+		
+		
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//Drivers//geckodriver.exe");
+		driver = new FirefoxDriver(option);
+		driver.get("http://demo.automationtesting.in/FileDownload.html");
+		driver.findElement(By.id("textbox")).sendKeys("text for txt file");
+		driver.findElement(By.id("createTxt")).click();
+		driver.findElement(By.id("link-to-download")).click();
+		driver.close();
+
+	}
+
+}
