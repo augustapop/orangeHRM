91e6d1c898afed54173c6652714c0b946841d5d7
augustapop <augustapop@users.noreply.github.com>
2020-02-05 11:32:31 +0200

    1 commit
    1 commit

--- src/test/java/POMOrange/LeavePageOrange.java
+++ src/test/java/POMOrange/LeavePageOrange.java
@@ -1,5 +1,26 @@
 package POMOrange;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class LeavePageOrange {
 
+	//https://opensource-demo.orangehrmlive.com/index.php/leave/viewLeaveList
+	WebDriver driver;
+	
+	@FindBy(how=How.ID,using="calFromDate") WebElement txtFromDate;
+	@FindBy(how=How.ID,using="calToDate") WebElement txtToDate;
+	@FindBy(how=How.XPATH,using="checkbox_group label_first") WebElement checkAll;
+	//xpath://label[contains(text(),"All")]
+	//label[contains(text(),"Cancelled")]
+	@FindBy(how=How.ID,using="leaveList_txtEmployee_empName") WebElement leaveList_txtEmployee_empName;
+	@FindBy(how=How.ID,using="leaveList_cmbSubunit") WebElement leaveListSubUnit;
+	@FindBy(how=How.ID,using="leaveList_cmbWithTerminated") WebElement subUnitcheck;
+	
+	//Add Leave Entitlement
+	//https://opensource-demo.orangehrmlive.com/index.php/leave/addLeaveEntitlement?savedsearch=1 ;pentru windows appeare
+	
+	
 }
