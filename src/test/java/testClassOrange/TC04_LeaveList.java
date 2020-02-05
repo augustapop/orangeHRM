d42a3a3c43ffbbf13b6439db8ef260cd441feb35
augustapop <augustapop@users.noreply.github.com>
2020-02-05 16:57:07 +0200

    3rd modification
    3rd modification

--- src/test/java/testClassOrange/TC04_LeaveList.java
+++ src/test/java/testClassOrange/TC04_LeaveList.java
@@ -1,9 +1,9 @@
 package testClassOrange;
 
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import POMOrange.LeavePageOrange;
 
@@ -17,14 +17,24 @@ public class TC04_LeaveList extends BaseClassOrange {
 		List<WebElement> col = driver.findElements(By.xpath("//*[@id='resultTable']/thead/tr/th"));
 		int noCol = col.size();
 		System.out.println("Nr de coloane" + noCol);
-		String textCol = col.toString();
-		List<WebElement> rows = driver.findElements(By.xpath("//*[@id='resultTable']/thead/tr"));
-		for (int i = 0; i < noCol; i++) {
-			for (WebElement row : rows) {
-
-				if (textCol.contains("Pending")) {
-
-					driver.findElement(By.className("select_action quotaSelect")).click();
+		// String textCol = col.toString();
+		List<WebElement> rows = driver
+				.findElements(By.xpath("/html/body/div[1]/div[3]/div[2]/div/form/div[3]/table/tbody/tr"));
+		System.out.println("Rows number it is:" + rows.size());
+		/// html/body/div[1]/div[3]/div[2]/div/form/div[3]/table/tbody/tr[2]/td[1]
+		/// html/body/div[1]/div[3]/div[2]/div/form/div[3]/table/tbody/tr[2]/td[6]
+		for (int i = 0; i < rows.size(); i++) {
+			{
+				String text = driver.findElement(By.xpath("html/body/div[1]/div[3]/div[2]/div/form/div[3]/table/tbody/tr[" + (i + 1) + "]/td[6]"))
+						.getText();
+				System.out.println("Textul aferent este:" + text);
+				if (text.contains("Pending")) {
+					System.out.println("I am where I want to be");
+					driver.findElement(By.xpath("//*[@id='select_leave_action_2']")).click();
+					Select status=new Select(driver.findElement(By.xpath("//*[@id='select_leave_action_2']")));
+					status.selectByVisibleText("Cancel");
+					driver.findElement(By.name("btnSave")).click();
+					break;
 				}
 			}
 		}
